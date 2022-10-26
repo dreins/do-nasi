ffbdde086e707dc5f31c5eb336650003bbe88718
dreins <davynreinhard@gmail.com>
2022-10-26 18:56:16 +0700

    Views and Models

--- harapan_page/models.py
+++ harapan_page/models.py
@@ -1,15 +1,16 @@
 from email.policy import default
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.conf import settings
 
 class HarapanDonatur(models.Model):
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
+        get_user_model(),
         on_delete=models.CASCADE,
         null = True,
         blank = True
     )
-    date = models.DateField(auto_now=True)
-    title = models.CharField(max_length=200)
-    description = models.TextField()
+    created_at = models.DateField(auto_now=True)
+    text = models.TextField()
     harapan_image = models.ImageField(upload_to='harapan_image', default=None)
+    likes = models.IntegerField(default=0)
--- harapan_page/views.py
+++ harapan_page/views.py
@@ -1,5 +1,23 @@
+from cgitb import text
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from harapan_page.models import HarapanDonatur
+from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 # Create your views here.
+# @login_required(login_url='/todolist/login/')
+@csrf_exempt
 def harapan_page(request):
-    return render(request, 'harapan_page.html')
+    if request.method == 'POST':
+        user = request.user
+        text = request.POST.get('text')
+        harapan_image = request.POST.get('harapan_image')
+        harapan = HarapanDonatur.objects.create(user = user, text = text, harapan_image = harapan_image)
+        if harapan == None:
+            messages.info(request, 'Silahkan masukkan harapan anda!')
+        else:
+            return JsonResponse({'message : Harapan Created!'})
+        
+    return JsonResponse({'message : Harapan Uploaded!'})
